a540ecfad45b95d5da22eec6db8eaf2d0b3cf149
aliceugbah <aliceugbah@gmail.com>
2025-12-18 00:49:57 +0000

    Add files via upload

---
+++ tests/test_library_systemapp.py
@@ -0,0 +1,54 @@
+import unittest
+import app
+
+class TestLibrarySystem(unittest.TestCase):
+    @classmethod                                                # Must be decorated as class method 
+    def setUpClass(cls): 
+        app.books.clear()
+        app.borrowed.clear()                                       # setUpClass() class method is called before the tests (in an individual class) are run. It has class as the only argument
+        app.books.append ({"author":"Roger Burke", "title": "Rose", "category": "Romance", "available": True})
+        app.books.append ({"author":"David Ram", "title": "Yellow", "category":"Fiction", "available": True})
+        return super().setUpClass()
+    
+
+    @classmethod                                                # Must be decorated as class method
+    def tearDownClass(cls):                                     # tearDownClass() class method is called after all tests (in an individual class) have run. It has class as the only argument
+        print("\n\nTear down a class")
+        print("Releasing the resources")
+        return super().tearDownClass()
+    
+    # ----- ADD / VIEW BOOKS -----
+
+    def test_book_added(self):
+        self.assertEqual(len(app.books), 2)
+
+    def test_view_books(self):
+        self.assertTrue(len(app.books) > 0)
+
+     # ----- SEARCH BOOK -----
+    def test_search_books(self):
+        self.assertEqual(app.books[0]["title"], "Rose")
+    
+
+    # -----RETURN BOOK -------
+    def test_return_books(self):
+        app.books[0]["available"] = True
+        self.assertTrue(app.books[0]["available"])
+
+
+    # -----AVAILABILITY BOOK -------   
+    def test_book_availablity(self):
+        self.assertTrue(app.books[0]["available"])
+
+    # -----BORROW BOOK -------
+    def test_borrow_books(self):
+        app.books[0]["available"] = False
+        self.assertFalse(app.books[0]["available"])
+    
+        # -----DELETE BOOK -------
+    def test_delete_books(self):
+        app.books.pop()
+        self.assertEqual(len(app.books), 1)
+
+if __name__ == "__main__":
+    unittest.main()
